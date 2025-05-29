63dbaafae69b86f001fd7aca0c14b0857dfa0e30
kjy <rlawndus4801@naver.com>
2025-05-29 23:35:04 +0900

    fix indentation

--- src/de_model.hpp
+++ src/de_model.hpp
@@ -13,51 +13,52 @@
 #include <vector>
 
 namespace de {
-    class DeModel {
-        public:
-        struct Vertex {
-            glm::vec3 position{};
-            glm::vec3 color{};
-            glm::vec3 normal{};
-            glm::vec2 uv{};
+class DeModel {
+  public:
+    struct Vertex {
+      glm::vec3 position{};
+      glm::vec3 color{};
+      glm::vec3 normal{};
+      glm::vec2 uv{};
 
-            static std::vector<VkVertexInputBindingDescription> getBindingDescriptions();
-            static std::vector<VkVertexInputAttributeDescription> getAttributeDescriptions();
+      static std::vector<VkVertexInputBindingDescription> getBindingDescriptions();
+      static std::vector<VkVertexInputAttributeDescription> getAttributeDescriptions();
 
-            bool operator==(const Vertex &other) const {
-                return position == other.position && color == other.color && normal == other.normal && uv == other.uv;
-            }
-        };
-
-        struct Builder {
-            std::vector<Vertex> vertices{};
-            std::vector<uint32_t> indices{};
-
-            void loadModel(const std::string &filepath);
-        };
-
-        DeModel(DeDevice &device, const DeModel::Builder &builder);
-        ~DeModel();
-
-        DeModel(const DeModel &) = delete;
-        DeModel & operator=(const DeModel &) = delete;
-
-        static std::unique_ptr<DeModel> createModelFromFile(DeDevice &device, const std::string &filepath);
-
-        void bind (VkCommandBuffer commandBuffer);
-        void draw(VkCommandBuffer commandBuffer);
-
-        private:
-        void createVertexBuffers(const std::vector<Vertex> &vertices);
-        void createIndexBuffers(const std::vector<uint32_t> &indices);
-
-        DeDevice &deDevice;
-
-        std::unique_ptr<DeBuffer> vertexBuffer;
-        uint32_t vertexCount;
-
-        bool hasIndexBuffer = false;
-        std::unique_ptr<DeBuffer> indexBuffer;
-        uint32_t indexCount;
+      bool operator==(const Vertex &other) const {
+        return position == other.position && color == other.color && normal == other.normal && uv == other.uv;
+      }
     };
+
+    struct Builder {
+      std::vector<Vertex> vertices{};
+      std::vector<uint32_t> indices{};
+
+      void loadModel(const std::string &filepath);
+    };
+
+    DeModel(DeDevice &device, const DeModel::Builder &builder);
+    ~DeModel();
+
+    DeModel(const DeModel &) = delete;
+    DeModel & operator=(const DeModel &) = delete;
+
+    static std::unique_ptr<DeModel> createModelFromFile(DeDevice &device, const std::string &filepath);
+
+    void bind(VkCommandBuffer commandBuffer);
+    void draw(VkCommandBuffer commandBuffer);
+
+  private:
+    void createVertexBuffers(const std::vector<Vertex> &vertices);
+    void createIndexBuffers(const std::vector<uint32_t> &indices);
+
+    DeDevice &deDevice;
+
+    std::unique_ptr<DeBuffer> vertexBuffer;
+    uint32_t vertexCount;
+
+    bool hasIndexBuffer = false;
+    std::unique_ptr<DeBuffer> indexBuffer;
+    uint32_t indexCount;
+};
+
 }  // namespace de
