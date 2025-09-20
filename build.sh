4d29097713697a64aad701e5037187d363f82e62
kjy <rlawndus4801@naver.com>
2025-09-20 17:41:58 +0900

    add initial build argument

--- build.sh
+++ build.sh
@@ -1,7 +1,27 @@
 #!/usr/bin/env bash
 
 # rm -rf build
-mkdir -p build
-cd build
+# mkdir -p build
+# cd build
+# cmake .. -DCMAKE_BUILD_TYPE=Debug ..
+# cmake --build .
+
+set -e
+SCRIPT_DIR=$(dirname ${BASH_SOURCE})
+
+INITIAL_BUILD=false
+for arg in "$@"; do
+  if [ "$arg" == "--initial" ]; then
+    INITIAL_BUILD=true
+    break
+  fi
+done
+
+if [ "$INITIAL_BUILD" == "true" ]; then
+  rm -rf $SCRIPT_DIR/build
+  mkdir $SCRIPT_DIR/build
+fi
+
+cd $SCRIPT_DIR/build
 cmake .. -DCMAKE_BUILD_TYPE=Debug ..
 cmake --build .
