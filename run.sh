da709f323145e0063be21355e1adb3a65859efa8
kjy <rlawndus4801@naver.com>
2025-09-20 17:42:06 +0900

    add debug argument

--- run.sh
+++ run.sh
@@ -1,3 +1,20 @@
 #!/usr/bin/env bash
 
-./build/samples/samples
+# ./build/samples/samples
+
+set -e
+SCRIPT_DIR=$(dirname ${BASH_SOURCE})
+
+DEBUG=false
+for arg in "$@"; do
+  if [ "$arg" == "--debug" ]; then
+    DEBUG=true
+    break
+  fi
+done
+
+if [ "$DEBUG" == "true" ]; then
+  gdb $SCRIPT_DIR/build/samples/samples
+else
+  $SCRIPT_DIR/build/samples/samples
+fi
